fde4df33c1ea96bb5eece16eb89b1a974bc3d63f
NakulKachhava <24010101116@darshan.ac.in>
2026-07-14 13:29:01 +0530

    Add files via upload

---
+++ lab4/A6.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+
+#define v 3
+int adm[v][v];
+int dist[v][v];
+
+void bfs(int src){
+    int que[v];
+
+
+    int rear = -1, front = -1;
+
+    int visited[v];
+
+    for (int i = 0; i < v; i++)
+    {
+        visited[i] = 0;
+    }
+
+    visited[src] = 1;
+    que[++rear] = src;
+
+    while (front<rear)
+    {
+        int current = que[++front];
+        for (int i=0; i < v; i++){
+            if (adm[current][i] == 1 && visited[i] == 0){
+                visited[i] = 1;
+                dist[src][i] = dist[src][current] + 1;
+                que[++rear] = i;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int e;
+
+    printf("Enter no. of edges: ");
+    scanf("%d",&e);
+
+    if (e != v-1)
+    {
+        printf("The Given Graph is not a Tree.");
+        return 0;
+    }
+    
+    int visited[v];
+    int edges[e][2];
+    
+    for (int i = 0; i < e; i++)
+    {
+        printf("Enter edge %d: ",i);
+        scanf("%d %d",&edges[i][0],&edges[i][1]);
+    }
+
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            adm[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            dist[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < e; i++)
+    {
+        adm[edges[i][0]][edges[i][1]] = 1;
+        adm[edges[i][1]][edges[i][0]] = 1;
+    }
+
+    for (int i = 0; i < v; i++)
+    {
+        bfs(i);
+    }
+
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            if (dist[i][i] != 0)
+            {
+                printf("The Given Graph is not a Tree.");
+                return 0;
+            }
+            
+            if (dist[i][j] == 0 && i != j)
+            {
+                printf("The Given Graph is not a Tree.");
+                return 0;
+            }
+        }
+    }
+    
+    printf("The Given Graph is a Tree.");
+
+    return 0;
+}
